82ba6c47dc17b94f1493e4fb5ae871c9ed37c509
ajay b <ajutech6@gmail.com>
2025-08-10 12:34:41 +0530

    Update script.js

--- my ads webpage/script.js
+++ my ads webpage/script.js
@@ -1,4 +1,9 @@
+const toggleBtn = document.getElementById("menu-toggle");
+const menuPanel = document.getElementById("menu-panel");
 
+toggleBtn.addEventListener("click", () => {
+  menuPanel.classList.toggle("active");
+});
 // Rating stars functionality
 const stars = document.querySelectorAll('.stars .star');
 const ratingResult = document.querySelector('.rating-result');
@@ -65,7 +70,15 @@ commentForm.addEventListener('submit', (e) => {
   // Scroll to new comment
   li.scrollIntoView({ behavior: 'smooth' });
 });
+document.getElementById("dark-mode-toggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
 
-
-
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDDS5Wj6o843817kUpNTK4gLKKUFcMn5-Q",
+  authDomain: "mymoviescomments.firebaseapp.com",
+  projectId: "mymoviescomments",
+  storageBucket: "mymoviescomments.firebasestorage.app",
+  messagingSenderId: "460470112788",
+  appId: "1:460470112788:web:a8ceece7ad5b13c79f0d01"
+};
